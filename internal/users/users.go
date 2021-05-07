4e45133c07d7f3017c7ef1ba9cdfcac8451edfc2
ext-minh.truong <ext-minh.truong@spdigital.sg>
2021-05-07 12:39:25 +0700

    User SignUp and Login Functionality

--- internal/users/users.go
+++ internal/users/users.go
@@ -1,7 +1,39 @@
 package users
 
+import (
+	"golang.org/x/crypto/bcrypt"
+	"log"
+
+	database "github.com/truongcongminh96/graphql-golang-example/internal/pkg/db/mysql"
+)
+
 type User struct {
 	ID       string `json:"id"`
 	Username string `json:"name"`
 	Password string `json:"password"`
+}
+
+func (user *User) Create() {
+	statement, err := database.Db.Prepare("INSERT INTO Users(Username,Password) VALUES(?,?)")
+	print(statement)
+	if err != nil {
+		log.Fatal(err)
+	}
+	hashedPassword, err := HashPassword(user.Password)
+	_, err = statement.Exec(user.Username, hashedPassword)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+//HashPassword hashes given password
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+// CheckPasswordHash CheckPassword hash compares raw password with it's hashed values
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
