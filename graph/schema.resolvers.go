486ce49a16548343fd497295aa95f351f88c7505
ext-minh.truong <ext-minh.truong@spdigital.sg>
2021-05-07 11:40:15 +0700

    Refactor links query

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -15,10 +15,12 @@ import (
 
 func (r *mutationResolver) CreateLink(ctx context.Context, input model.NewLink) (*model.Link, error) {
 	var link links.Link
+
 	link.Title = input.Title
 	link.Address = input.Address
 	linkID := link.Save()
-	return &model.Link{ID: strconv.FormatInt(linkID, 10), Title:link.Title, Address:link.Address}, nil
+
+	return &model.Link{ID: strconv.FormatInt(linkID, 10), Title: link.Title, Address: link.Address}, nil
 }
 
 func (r *mutationResolver) CreateUser(ctx context.Context, input model.NewUser) (string, error) {
@@ -34,14 +36,16 @@ func (r *mutationResolver) RefreshToken(ctx context.Context, input model.Refresh
 }
 
 func (r *queryResolver) Links(ctx context.Context) ([]*model.Link, error) {
-	var links []*model.Link
-	dummyLink := model.Link{
-		Title:   "our dummy link",
-		Address: "https://address.org",
-		User:    &model.User{Name: "admin"},
+	var resultLinks []*model.Link
+	var dbLinks []links.Link
+
+	dbLinks = links.GetAll()
+
+	for _, link := range dbLinks {
+		resultLinks = append(resultLinks, &model.Link{ID: link.ID, Title: link.Title, Address: link.Address})
 	}
-	links = append(links, &dummyLink)
-	return links, nil
+
+	return resultLinks, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
--- internal/links/links.go
+++ internal/links/links.go
@@ -1,6 +1,7 @@
 package links
 
 import (
+	"database/sql"
 	"log"
 
 	database "github.com/truongcongminh96/graphql-golang-example/internal/pkg/db/mysql"
@@ -22,16 +23,62 @@ func (link Link) Save() int64 {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	//#4
 	res, err := stmt.Exec(link.Title, link.Address)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	//#5
 	id, err := res.LastInsertId()
 	if err != nil {
 		log.Fatal("Error:", err.Error())
 	}
+
 	log.Print("Row inserted!")
+
 	return id
+}
+
+func GetAll() []Link {
+	stmt, err := database.Db.Prepare("select id, title, address from Links")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func(stmt *sql.Stmt) {
+		err := stmt.Close()
+		if err != nil {
+
+		}
+	}(stmt)
+
+	rows, err := stmt.Query()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func(rows *sql.Rows) {
+		err := rows.Close()
+		if err != nil {
+
+		}
+	}(rows)
+
+	var links []Link
+	for rows.Next() {
+		var link Link
+		err := rows.Scan(&link.ID, &link.Title, &link.Address)
+		if err != nil {
+			log.Fatal(err)
+		}
+		links = append(links, link)
+	}
+
+	if err = rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return links
 }
